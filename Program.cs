fea225ca1413f1ba3fd5669c25c8568c677a22fd
Augusto Maia <augustofmaia@hotmail.com>
2022-09-14 09:31:39 -0300

    Criando classe lista

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿OperacaoInicial();
+
+void OperacaoInicial(){
+    while(true){
+        Console.WriteLine("Imforme a operação desejada.");
+        Console.WriteLine("1 - Criar lista");
+        Console.WriteLine("2 - Editar lista");
+        var operacaoDesejada = Console.ReadLine();
+        if(operacaoDesejada == "1")
+            OperacaoCriarLista();
+        else if(operacaoDesejada == "2")
+            OperacaoEditarLista();
+        else
+            OperacaoInicial();
+    }    
+}
+
+void OperacaoCriarLista(){
+    Console.WriteLine("Nome: ");
+    var nome = Console.ReadLine();
+    Console.WriteLine("Data: ");
+    var data = Console.ReadLine();
+    OperacaoInicial();
+}
+
+void OperacaoEditarLista(){
+    while(true){
+        Console.WriteLine("Imforme a operação desejada.");
+        Console.WriteLine("1 - adicionar itens");
+        Console.WriteLine("2 - Editar itens");
+        var operacaoDesejada = Console.ReadLine();
+    }    
+}
+
+void OperacaoAdicionarItens(){
+    Console.WriteLine("Nome: ");
+    var nome = Console.ReadLine();
+    Console.WriteLine("Data: ");
+    var data = Console.ReadLine();
+    OperacaoInicial();
+}
