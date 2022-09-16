70d6d55163be7af06bd3a680e96f618fdf14ff5e
Augusto Maia <augustofmaia@hotmail.com>
2022-09-16 11:36:58 -0300

    Operação editar lista

--- modelo/Lista.cs
+++ modelo/Lista.cs
@@ -3,10 +3,15 @@ namespace Modelo;
 public class Lista {
     public readonly string Nome;
     public DateTime DataDesejadaDaCompra { get; private set; }
-
+    public List<Produto> Produtos {get; private set; } = new List<Produto>();
     public Lista(string nome, DateTime dataDesejadaDaCompra)
     {
         Nome = nome;
         DataDesejadaDaCompra = dataDesejadaDaCompra;
     }
+
+    public void Adicionar(Produto produto){
+        Produtos.Add(produto);
+    }
+    
 }
